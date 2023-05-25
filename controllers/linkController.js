dfc203124078841f1effca3fe20970684f341497
Ivan Aponza <joselito881218@hotmail.com>
2023-05-25 16:50:21 -0500

    Actualizar links

--- controllers/linkController.js
+++ controllers/linkController.js
@@ -73,3 +73,35 @@ export const removeLink = async (req, res) => {
     return res.status(500).json({ error: "Error de servidor" });
   }
 };
+
+export const updateLink = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { longLink } = req.body;
+
+    //console.log(longLink);
+
+    if (!longLink.startsWith("https://")) {
+      longLink = "https://" + longLink;
+    }
+
+    const link = await Link.findById(id);
+
+    if (!link) return res.status(404).json({ error: "No existe link" });
+
+    if (!link.uid.equals(req.uid))
+      return res.status(401).json({ error: "No le pertenece el id" });
+
+    //actualizar
+    link.longLink = longLink;
+    await link.save();
+
+    return res.json({ link });
+  } catch (error) {
+    console.log(error);
+    if (error.kind === "ObjectId") {
+      return res.status(403).json({ error: "Formato id incorrecto" });
+    }
+    return res.status(500).json({ error: "Error de servidor" });
+  }
+};
--- routes/link.route.js
+++ routes/link.route.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { createLink, getLink, getLinks, removeLink } from "../controllers/linkController.js";
+import { createLink, getLink, getLinks, removeLink, updateLink } from "../controllers/linkController.js";
 import { requireToken } from "../middlewares/requireToken.js";
 import { bodyLinkValidator, paramLinkValidator } from "../middlewares/validatorManager.js";
 const router = Router();
@@ -16,5 +16,6 @@ router.get('/', requireToken, getLinks);
 router.get('/:id', requireToken, getLink);
 router.post('/', requireToken, bodyLinkValidator, createLink);
 router.delete('/:id', requireToken, paramLinkValidator, removeLink);
+router.patch('/:id', requireToken, paramLinkValidator, bodyLinkValidator, updateLink)
 
 export default router;
